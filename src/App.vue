aa16fc1f468955d31552428841e03b1bf25dd021
jason.wang <1045643440@qq.com>
2017-07-22 05:16:52 +0800

    初始化简历项目

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    {{  message }}
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+
+  },
+  data() {
+    return {
+        message: "this is a message"
+    }
+  },
+  created(){
+    var n =0 ;
+    setInterval(()=>{
+        this.message = n;
+        n += 1;
+    }, 1000)
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
